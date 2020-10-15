c519fe6025d7a25f1ca4c35227234327069a2e6a
Edaz <edazpotato@gmail.com>
2020-10-16 12:27:57 +1300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,59 @@
+function calc () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    love = randint(0, 100)
+    basic.showString("" + (`Love.${love}%`))
+}
+input.onButtonPressed(Button.A, function () {
+    pressed.a = true;
+})
+input.onButtonPressed(Button.B, function () {
+    pressed.b = true;
+})
+function start () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+}
+function reset () {
+    pressed = {
+        a: false,
+        b: false
+    }
+}
+let love = 0
+let pressed = {
+    a: false,
+    b: false
+}
+start()
+basic.forever(function () {
+    if (pressed.a == true && pressed.b == true) {
+        calc()
+        reset()
+        start()
+    }
+})
